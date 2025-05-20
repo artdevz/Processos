339043862540df1c9d64be0c9a3af78311254b02
bruno20614 <brunocdl9@gmail.com>
2025-05-20 15:55:55 -0300

    Feito o sjf

--- quest01/src/Scheduler.cpp
+++ quest01/src/Scheduler.cpp
@@ -43,6 +43,45 @@ void Scheduler::firstComeFirstServe() {
     std::cout << "Todos os processos foram executados!\n";
 }
 
+void Scheduler::shortestJobFirst(){
+    int time = 0;
+
+    int n ;
+    printf("Processos que tem");
+    scanf("%d" , &n);
+
+    Process p[n];
+
+    for (int i = 0; i < n; i++){
+        p[i].pid = i + 1;
+        printf("Processo", p[i].pid);
+        scanf("%d %d", &p[i].arrivalTime, &p[i].burstTime);
+    }
+
+    int completed = 0;
+    int current_time = 0;
+while (completed < n)
+{
+    int indice = 0;
+    int mini_burst = 0;
+
+    for( int i = 0; i < n ; i++){
+        if (!p[i].done && p[i].arrivalTime <= current_time){
+            if(p[i].burstTime < mini_burst){
+                mini_burst = p[i].burstTime;
+                indice = i;
+            }
+        }
+    }
+
+    if (indice == 0){
+        current_time ++;
+        continue;
+    }
+}
+
+}
+
 void Scheduler::shortestJobFirst() {
     std::cout << "[SJF executado]\n";
 }
